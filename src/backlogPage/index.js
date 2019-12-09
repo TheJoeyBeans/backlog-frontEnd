7462eeab949479c322a7bb8fb506749ab5596574
Joseph Benbella <47164559+TheJoeyBeans@users.noreply.github.com>
2019-12-08 18:47:57 -0700

    getting backlog page ready

---
+++ src/backlogPage/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Image, Row, Col, Container, Card, Button } from 'react-bootstrap';
+
+function backlogPage(props){
+	const gameItem = props.gameResults.map((game, i) =>{
+		return(
+			<Col md={4} key={i}>
+				<Card className="gameCard" >
+					<Card.Img className="gameCardImage" variant='top' src={game.pic} />
+					<Card.Title className="gameCardTitle">{game.title}</Card.Title>
+					<Button onClick={(e) => props.grabId(game.id)} variant='primary'>Add To Backlog</Button>
+				</Card>
+			</Col>
+		)
+	});
+	return (
+		<Container>
+			<Row>
+				{gameItem}
+			</Row>
+		</Container>
+	)
+}
+
+
+export default backlogPage;
