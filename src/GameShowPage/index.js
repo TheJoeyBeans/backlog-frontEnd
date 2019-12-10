56b3d7cfba32651413fd7cca362a116946e34642
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-12-10 16:58:45 -0700

    game show page

--- src/GameShowPage/index.js
+++ src/GameShowPage/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Image, Row, Col, Container, Card, Button, Navbar, Dropdown, DropdownButton } from 'react-bootstrap';
 
 class GameShowPage extends Component{
 	constructor(props){
 		super(props)
 
 		this.state ={
-			
+			publishers: [],
+			developers: []
 		}
 	}
 
@@ -28,7 +30,7 @@ class GameShowPage extends Component{
 				developers: response.data.developers,
 				publishers: response.data.publishers,
 				image: response.data.background_image,
-				gameDescription: response.data.description,
+				gameDescription: response.data.description_raw,
 				ratings: response.data.ratings
 			})
 		})
@@ -37,7 +39,41 @@ class GameShowPage extends Component{
 	render(){
 		return(
 			<div>
-				
+				<Navbar sticky="top" variant="dark" bg='dark'>	
+					<Col>
+						<Navbar.Brand href='/'>Backlog</Navbar.Brand>
+					</Col>
+					<Col>
+					</Col>
+					<Col>
+						<DropdownButton title='Dropdown' className='headerDropDown'>
+							<Dropdown.Item>Profile</Dropdown.Item>
+							<Dropdown.Item href='/completedGames'>Completed Games</Dropdown.Item>
+							<Dropdown.Item onClick={this.props.logout}>LogOut</Dropdown.Item>
+						</DropdownButton>
+					</Col>			
+				</Navbar>
+				<Container>
+					<Row>
+						<Col>
+							<h1 id='gameShowPageTitle'>{this.state.gameTitle}</h1>	
+						</Col>
+					</Row>
+					<div id='gameShowPageBody'>
+					<Row>
+							<Col>
+								<div>
+									<img id='gameShowPageImage' src={this.state.image}/>
+								</div>
+							</Col>
+							<Col md={7}>
+								<div id='gameShowPageDescription'>
+									{this.state.gameDescription}
+								</div>
+							</Col>	
+					</Row>
+					</div>
+				</Container>
 			</div>
 		)
 	}
